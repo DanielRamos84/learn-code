d2958d7697e4d5b7f3e189a88d550f0c6ebda804
DanielRamos84 <61128414+DanielRamos84@users.noreply.github.com>
2020-02-27 19:52:48 -0800

    Update notes.js

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,3 +1,5 @@
+const notes= getSavedNotes()
+
 const filters={
 searchText: ''
 }
@@ -5,12 +7,14 @@ searchText: ''
 renderNotes(notes, filters)
 
 document.getElementById("create-note").addEventListener('click', function(e){
+  const createNoteIdBtn= uuidv4()
   notes.push({
+    id: createNoteIdBtn,
     title: '',
     body: ''
   })
 localStorage.setItem('notes', JSON.stringify(notes))
-renderNotes(notes, filters)
+location.assign(`/edit.html#${createNoteIdBtn}`)
 })
 
 document.getElementById("search-text").addEventListener('input', function(e){
