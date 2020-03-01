180a350947e12316635d9497b0d48859d6511469
DanielRamos84 <61128414+DanielRamos84@users.noreply.github.com>
2020-03-01 14:27:12 -0800

    Update notes-edits.js

--- notes-app/notes-edits.js
+++ notes-app/notes-edits.js
@@ -9,3 +9,22 @@ if (note === undefined){
     location.assign('/index.html')
 }
 
+const noteTitle= document.getElementById("note-title")
+noteTitle.addEventListener('input', function(e){
+    note.title= e.target.value
+    localStorage.setItem('notes', JSON.stringify(notes))
+})
+
+const textBody= document.getElementById("text-body")
+textBody.addEventListener('input', function(e){
+    note.body= e.target.value
+    localStorage.setItem('notes', JSON.stringify(notes))
+    getSavedNotes(notes)
+})
+
+const remBtn= document.getElementById("remove-note")
+remBtn.addEventListener('click', function (e){
+    removeNote(note.id)
+    location.assign('/index.html')
+    getSavedNotes(notes)
+})
