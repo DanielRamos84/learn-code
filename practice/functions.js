0149ccc81e108cb9cc6d3a72d712ff129a586cc1
DanielRamos84 <61128414+DanielRamos84@users.noreply.github.com>
2020-03-28 07:33:14 -0700

    Create functions.js

---
+++ practice/functions.js
@@ -0,0 +1,10 @@
+//Begin check if data avaiable in localStorage set array to use this data
+const getSavedNotes= function (){
+const userSaved= localStorage.getItem('keysSaved')
+if (userSaved!==null) {
+     return JSON.parse(userSaved) //Note we no longer make assignment to vehicleArray but instead we return value to the function so it can be called in a different file
+} else {
+     return [] //Note we no longer set vehicleArray to empty object but instead we return value to the function so it can be called in a different file
+}
+}
+//End check if data avaiable in localStorage set array to use this data
