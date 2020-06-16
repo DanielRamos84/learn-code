d61f959c56cf4e3a52eabe117de30324a29f562a
DanielRamos84 <61128414+DanielRamos84@users.noreply.github.com>
2020-06-16 15:11:44 -0700

    Update dom.js

--- Guess_num/dom.js
+++ Guess_num/dom.js
@@ -2,6 +2,7 @@ const submitNumber= document.getElementById('submit-number')
 const enterValue= document.getElementById('enter-value')
 const resultSection= document.getElementById('result-section')
 const countSection= document.getElementById('count-section')
+const resetButton= document.getElementById('reset-button')
 
 submitNumber.addEventListener('click', function(e){
     e.preventDefault()
@@ -9,4 +10,12 @@ submitNumber.addEventListener('click', function(e){
     countSection.textContent=''
     const userNumInput= enterValue.value.trim()
     userGuess(userNumInput)
+})
+
+resetButton.addEventListener('click', function(e){
+    e.preventDefault()
+    localStorage.clear()
+    countSection.textContent=''
+    resetField()
+    resultSection.textContent=''
 })
